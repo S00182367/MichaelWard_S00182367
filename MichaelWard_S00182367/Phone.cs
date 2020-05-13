9fe6f9a342073efffda38a93454f65b8a227905d
Mike Ward <s00182367@mail.itsligo.ie>
2020-05-13 12:40:24 +0100

    Packages dose not seem to install properly

--- MichaelWard_S00182367/Phone.cs
+++ MichaelWard_S00182367/Phone.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace MichaelWard_S00182367
 {
@@ -18,10 +19,15 @@ namespace MichaelWard_S00182367
 
         public string Phone_Image { get; set; }
 
+        Phone p1 = new Phone("Samsung S20", 500, "Android", "/images/android.png", "/images/s20.jpg");
+        Phone p2 = new Phone("iPhone 11", 600, "IOS", "/images/apple.png", "/images/iphone11.jpg");
+
+        public void IncreasePrice(int precent)
+        {
+            Price * precent / 100;
+        }
+
     }
 
-    //public void IncreasePrice()
-    //{
-
-    //}
+   
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MichaelWard_S00182367;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPrecent()
+        {
+            //Arrange
+            Price pp1 = new Price();
+            int total = 105;
+
+            //Act
+            pp1 * 5 / 100;
+            //Assert
+        }
+    }
+}
