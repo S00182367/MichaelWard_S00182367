0c80dbefe03261c4df099b1ca72ee0aff1b28ad9
Mike Ward <s00182367@mail.itsligo.ie>
2020-05-13 10:32:35 +0100

    Part 2 Create a class complete

---
+++ MichaelWard_S00182367/Phone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MichaelWard_S00182367
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public string OperatingSystem { get; set; }
+
+        public string OS_Images { get; set; }
+
+        public string Phone_Image { get; set; }
+    }
+}
